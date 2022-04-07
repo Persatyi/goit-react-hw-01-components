f0f5429db9c9aeb3dfaf04ee32c5fafe30b069a5
Oleksii Persatyi <persatyi@ukr.net>
2022-04-08 01:33:15 +0300

    commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,17 +1,17 @@
+import Profile from './Profile/Profile';
+import user from 'User/user.json';
+import data from '../Data/data.json';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        textTransform: 'uppercase',
-        color: '#010101',
-      }}
-    >
-      React homework template
-    </div>
+    <Profile
+      username={user.username}
+      tag={user.tag}
+      location={user.location}
+      avatar={user.avatar}
+      stats={user.stats}
+    />
+    //     <Statistics title="Upload stats" stats={data} />
+    // <Statistics stats={data} />
   );
 };
